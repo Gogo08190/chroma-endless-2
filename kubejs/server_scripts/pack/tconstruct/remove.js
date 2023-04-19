c21f156ebdfd2cbeea8d43bbc39ad050fab7c444
Gogo08 <gogo08190@gmail.com>
2023-04-19 18:57:44 +0200

    [✨] Remove duplicate recipe tconstruct

---
+++ kubejs/server_scripts/pack/tconstruct/remove.js
@@ -0,0 +1,25 @@
+// priority: 100
+
+onEvent('recipes', (event) => {
+    const idRemovals = [
+      'tconstruct:tables/tinkers_anvil',
+      'tconstruct:smeltery/seared/drain_retextured',
+      'tconstruct:smeltery/seared/duct_retextured',
+      'tconstruct:smeltery/seared/chute_retextured',
+      'tconstruct:smeltery/scorched/duct_retextured',
+      'tconstruct:smeltery/scorched/drain_retextured',
+      'tconstruct:smeltery/scorched/chute_retextured',
+    ];
+
+    const outputRemovals = [
+
+    ];
+
+    idRemovals.forEach((id) => {
+        event.remove({ id: id });
+    });
+
+    outputRemovals.forEach((output) => {
+        event.remove({ output: output });
+    });
+});
