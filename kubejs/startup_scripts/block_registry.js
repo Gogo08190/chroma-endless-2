e2b2b785b5b651a3b215ba59252d1f84d34fd42e
Gogo08 <gogo08190@gmail.com>
2023-04-20 10:50:12 +0200

    [✨] Change items/block/fluid registry script

--- kubejs/startup_scripts/block_registry.js
+++ kubejs/startup_scripts/block_registry.js
@@ -2,6 +2,6 @@ onEvent('block.registry', (event) => {
     const blocks = [];
 
     blocks.forEach((block) => {
-        event.create(block.name).material(block.material).hardness(block.hardness);
+        event.create(block.name).group('Chroma').setRegistryName('chroma', block.name).material(block.material).hardness(block.hardness);
     });
 });
--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -6,6 +6,6 @@ onEvent('item.registry', (event) => {
     const reusableItemTextures = [];
 
     items.forEach((item) => {
-        event.create(item).group('KubeJS').texture(`kubejs:item/${item}`);
+        event.create(item).group('Chroma').setRegistryName('chroma', item).texture(`kubejs:item/${item}`);
     });
 });
