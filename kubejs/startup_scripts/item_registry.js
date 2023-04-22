4fe40a09be434a2d49d0604ad5affbd76b82b60a
Gogo08 <gogo08190@gmail.com>
2023-04-22 16:37:00 +0200

    [✨] Fix item registry

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -1,11 +1,7 @@
 onEvent('item.registry', (event) => {
     const items = [];
 
-    const ritualDummies = [];
-
-    const reusableItemTextures = [];
-
     items.forEach((item) => {
-        event.create(item).group('Chroma').setRegistryName('chroma', item).texture(`kubejs:item/${item}`);
+        event.create('chroma:' + item).texture(`kubejs:item/${item}`);
     });
 });
