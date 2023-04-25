bb99b43ad138c531e48b459f9121ce6bf509f4c1
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2023-04-25 14:55:31 +0200

    [🍱] Add andesite alloy block

--- kubejs/startup_scripts/block_registry.js
+++ kubejs/startup_scripts/block_registry.js
@@ -1,5 +1,7 @@
 onEvent('block.registry', (event) => {
-    const blocks = [];
+    const blocks = [
+      {name: 'andesite_alloy_block', displayName: 'Block of Andesite Alloy', material: 'iron', hardness: '5'},
+    ];
 
     blocks.forEach((block) => {
         event.create(block.name).group('Chroma').setRegistryName('chroma', block.name).material(block.material).hardness(block.hardness);
