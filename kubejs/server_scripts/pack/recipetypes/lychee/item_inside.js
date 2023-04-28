9786f5ee3d9989d509fe340bb93fe11b44195a60
Gogo08 <gogo08190@gmail.com>
2023-04-28 09:11:17 +0200

    [✨] Add recipe to moonstone and charoite with conditions

--- kubejs/server_scripts/pack/recipetypes/lychee/item_inside.js
+++ kubejs/server_scripts/pack/recipetypes/lychee/item_inside.js
@@ -1,19 +1,75 @@
 onEvent('recipes', (event) => {
     const id_prefix = 'chroma:pack/lychee/item_inside/';
     const recipes = [
-        // {
-        //     input: 'minecraft:leather',
-        //     block_in: {
-        //         blocks: ['chroma:molten_mana']
-        //     },
-        //     post: [
-        //         {
-        //             type: 'drop_item',
-        //             item: 'mna:vellum'
-        //         }
-        //     ],
-        //     id: `${id_prefix}moonstone_block_to_pearl`
-        // },
+        {
+            input: 'mna:ornate_table',
+            block_in: {
+                blocks: ['chroma:molten_mana']
+            },
+            post: [
+                {
+                    type: 'drop_item',
+                    item: 'mna:occulus'
+                }
+            ],
+            id: 'mna:occulus'
+        },
+        {
+            input: 'ae2:fluix_pearl',
+            block_in: {
+                blocks: ['chroma:molten_mana']
+            },
+            // Only check the conditions once, then end. Next check happens after 1 second
+            max_repeats: 1,
+            post: [
+                // If around noon, convert to charoite
+                {
+                    type: 'drop_item',
+                    item: 'blue_skies:charoite',
+                    contextual: [
+                      {
+                        type: 'time',
+                        value: { min: 4000, max: 8000 },
+                        period: 24000,
+                        description: 'recipes.lychee.item_inside.charoite.condition.description'
+                      }
+                    ]
+                },
+                // If around midnight, convert to moonstone
+                {
+                    type: 'drop_item',
+                    item: 'blue_skies:moonstone_shard',
+                    contextual: [
+                      {
+                        type: 'time',
+                        value: { min: 16000, max: 20000 },
+                        period: 24000,
+                        description: 'recipes.lychee.item_inside.moonstone.condition.description' 
+                      }
+                    ]
+                },
+                // Otherwise do nothing (prevents the item from being destroyed)
+                {
+                    type: 'prevent_default',
+                    contextual: [
+                        {
+                            type: 'and',
+                            contextual: [
+                                {
+                                    type: 'not',
+                                    contextual: { type: 'time', value: { min: 4000, max: 8000 }, period: 24000 }
+                                },
+                                {
+                                    type: 'not',
+                                    contextual: { type: 'time', value: { min: 16000, max: 20000 }, period: 24000 }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+            id: `${id_prefix}fluix_pearl_to_moonstone_or_charoite`
+        },
     ];
 
     recipes.forEach((recipe) => {
--- kubejs/server_scripts/pack/recipetypes/mana_and_artifice/shaped.js
+++ kubejs/server_scripts/pack/recipetypes/mana_and_artifice/shaped.js
@@ -11,6 +11,29 @@ onEvent('recipes', (event) => {
             },
             id: `${id_prefix}guide_book`
         },
+        {
+            output: 'mna:inscription_table',
+            pattern: [' C ', 'FBV', ' T '],
+            key: {
+                V: 'mna:vellum',
+                C: '#chipped:blue_carpet',
+                F: 'mna:animated_quill',
+                B: 'mna:guide_book',
+                T: 'mna:ornate_table'
+            },
+            id: 'mna:inscription_table'
+        },
+        {
+            output: 'mna:manaweaving_altar',
+            pattern: ['CVC', 'VSV', 'CDC'],
+            key: {
+                C: '#mna:gems/chimerite',
+                V: '#mna:ingots/vinteum',
+                S: 'tetra:forged_beam',
+                D: 'minecraft:polished_deepslate_slab'
+            },
+            id: 'mna:manaweaving_altar'
+        },
     ];
 
     recipes.forEach((recipe) => {
