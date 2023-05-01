6f790c4a3b39431a71813dc2513f49a413c951db
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2023-05-01 12:35:19 +0200

    [🍱] Add Gaia Block (texture-block)

--- kubejs/startup_scripts/block_registry.js
+++ kubejs/startup_scripts/block_registry.js
@@ -1,6 +1,7 @@
 onEvent('block.registry', (event) => {
     const blocks = [
       {name: 'andesite_alloy_block', displayName: 'Block of Andesite Alloy', material: 'stone', hardness: '0.4'},
+      {name: 'gaia_spirit_block', displayName: 'Block of Gaia Spirit Ingot', material: 'stone', hardness: '0.4'}
     ];
 
     blocks.forEach((block) => {
