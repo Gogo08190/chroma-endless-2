5bb644efe9d7b2d477ca9e6b809ac1b6aa9d751e
Gogo08 <gogo08190@gmail.com>
2023-05-03 18:48:10 +0200

    [✨] Silicon paste texture

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -3,7 +3,8 @@ onEvent('item.registry', (event) => {
       'mana_diamond_plate',
       'andesite_with_iron',
       'andesite_with_zinc',
-      'black_bone_meal'
+      'black_bone_meal',
+      'silicon_paste'
     ];
 
     items.forEach((item) => {
