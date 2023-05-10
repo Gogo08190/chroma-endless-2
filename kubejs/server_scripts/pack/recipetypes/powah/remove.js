a7a94232c15a901ef7127a904c179dc383ef4119
Gogo08 <gogo08190@gmail.com>
2023-05-10 16:03:37 +0200

    [✨] Edit remove.js powah

--- kubejs/server_scripts/pack/recipetypes/powah/remove.js
+++ kubejs/server_scripts/pack/recipetypes/powah/remove.js
@@ -1,6 +1,22 @@
 onEvent('recipes', (event) => {
-  event.remove({
-      output: '/powah:\\w+_starter/',
-      mod: 'powah'
-  });
+    const idRemovals = [
+
+    ];
+
+    const outputRemovals = [
+
+    ];
+
+    idRemovals.forEach((id) => {
+        event.remove({ id: id });
+    });
+
+    outputRemovals.forEach((output) => {
+        event.remove({ output: output });
+    });
+
+    event.remove({
+        output: '/powah:\\w+_starter/',
+        mod: 'powah'
+    });
 });
