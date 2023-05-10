80b3a5f6ffb0f49e4ffe7c99a4dadcb92322d88b
Thebloodshadow <45766546+Thebloodshadow@users.noreply.github.com>
2023-05-10 15:47:55 +0200

    [✨]Add explosion crafting pneuma + craft ender chests

---
+++ kubejs/server_scripts/pack/recipetypes/ender_storage/shaped.js
@@ -0,0 +1,45 @@
+onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:pack/ender_storage/shaped/';
+    const recipes = [
+      {
+        output: 'enderstorage:ender_chest',
+          pattern: ['BWB', 'OCO', 'BQB'],
+          key: {
+              B: 'forge:rods/blaze',
+              W: 'forge:wool',
+              O: 'forbidden_arcanus:obsidian_ingot',
+              C: 'forge:chests/wooden',
+              Q: 'create:polished_rose_quartz'
+          },
+          id: `${id_prefix}ender_chest`
+      },
+      {
+        output: 'enderstorage:ender_tank',
+          pattern: ['BWB', 'OCO', 'BQB'],
+          key: {
+              B: 'forge:rods/blaze',
+              W: 'forge:wool',
+              O: 'forbidden_arcanus:obsidian_ingot',
+              C: 'create:fluid_tank',
+              Q: 'create:polished_rose_quartz'
+          },
+          id: `${id_prefix}ender_tank`
+      },
+      {
+        output: 'enderstorage:ender_pouch',
+          pattern: ['BLB', 'LEL', 'BwB'],
+          key: {
+              B: 'forge:rods/blaze',
+              L: 'forge:leather',
+              E: 'minecraft:ender_eye',
+              W: 'forge:wool'
+          },
+          id: `${id_prefix}ender_pouch`
+      }
+      
+    ];
+
+    recipes.forEach((recipe) => {
+        event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
+    });
+});
---
+++ kubejs/server_scripts/pack/recipetypes/pneumaticcraft/explostion_crafting.js
@@ -0,0 +1,29 @@
+onEvent('recipes', (event) => {
+    /*
+          input : {item : ''} ou {tag: ''}
+          results : {item: ''} ou {tag: ''}
+          loss_rate : nb
+          id : `${id_prefix}id`
+    */
+
+    const id_prefix = 'chroma:pack/pneumaticcraft/explosion_crafting/';
+    const recipes = [
+        //Compressed Iron Ingot
+        {
+            input : {tag: '#forge:ingots/steel'},
+            results : {tag: '#forge:ingots/compressed_iron'},
+            loss_rate : 20,
+            id : `${id_prefix}compressed_iron_ingot`
+        }
+          
+    ];
+
+    recipes.forEach((recipe) => {
+        event.custom({
+                type: "pneumaticcraft:explosion_crafting",
+                input: recipe.input,
+                results:recipe.results,
+                loss_rate: recipe.loss_rate
+        })
+    });
+});
