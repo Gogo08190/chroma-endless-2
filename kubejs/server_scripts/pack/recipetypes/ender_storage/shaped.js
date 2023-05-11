2e115059066a724a67ddf920849fd2465d366391
Gogo08 <gogo08190@gmail.com>
2023-05-11 07:02:31 +0200

    [:bug:] Fix enderstorage recipes

--- kubejs/server_scripts/pack/recipetypes/ender_storage/shaped.js
+++ kubejs/server_scripts/pack/recipetypes/ender_storage/shaped.js
@@ -5,10 +5,10 @@ onEvent('recipes', (event) => {
         output: 'enderstorage:ender_chest',
           pattern: ['BWB', 'OCO', 'BQB'],
           key: {
-              B: 'forge:rods/blaze',
-              W: 'forge:wool',
+              B: '#forge:rods/blaze',
+              W: '#forge:wool',
               O: 'forbidden_arcanus:obsidian_ingot',
-              C: 'forge:chests/wooden',
+              C: '#forge:chests/wooden',
               Q: 'create:polished_rose_quartz'
           },
           id: 'enderstorage:ender_chest'
@@ -17,8 +17,8 @@ onEvent('recipes', (event) => {
         output: 'enderstorage:ender_tank',
           pattern: ['BWB', 'OCO', 'BQB'],
           key: {
-              B: 'forge:rods/blaze',
-              W: 'forge:wool',
+              B: '#forge:rods/blaze',
+              W: '#forge:wool',
               O: 'forbidden_arcanus:obsidian_ingot',
               C: 'create:fluid_tank',
               Q: 'create:polished_rose_quartz'
@@ -29,14 +29,14 @@ onEvent('recipes', (event) => {
         output: 'enderstorage:ender_pouch',
           pattern: ['BLB', 'LEL', 'BwB'],
           key: {
-              B: 'forge:rods/blaze',
-              L: 'forge:leather',
+              B: '#forge:rods/blaze',
+              L: '#forge:leather',
               E: 'minecraft:ender_eye',
-              W: 'forge:wool'
+              W: '#forge:wool'
           },
           id: 'enderstorage:ender_pouch'
       }
-      
+
     ];
 
     recipes.forEach((recipe) => {
