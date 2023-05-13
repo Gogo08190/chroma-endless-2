0a9e7dc0c96b3b962a4c76bf4c62ca9948cb076b
Gogo08 <gogo08190@gmail.com>
2023-05-13 13:46:44 +0200

    [✨] Unification

--- kubejs/server_scripts/pack/recipetypes/tags/chroma/blocks.js
+++ kubejs/server_scripts/pack/recipetypes/tags/chroma/blocks.js
@@ -1,3 +1,5 @@
 onEvent('block.tags', (event) => {
     event.add('forge:storage_blocks/andesite_alloy', 'chroma:andesite_alloy_block');
+    event.add('forge:storage_blocks/dragonstone', 'botania:dragonstone_block');
+    event.add('forge:storage_blocks/mana_diamond', 'botania:mana_diamond_block');
 });
