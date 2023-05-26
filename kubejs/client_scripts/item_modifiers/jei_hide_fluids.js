9dba3689d2adee169244c9010497e3ac57a3093f
Gogo08 <gogo08190@gmail.com>
2023-05-26 07:12:07 +0200

    [✨] Unification Oil

--- kubejs/client_scripts/item_modifiers/jei_hide_fluids.js
+++ kubejs/client_scripts/item_modifiers/jei_hide_fluids.js
@@ -1,6 +1,8 @@
 onEvent('jei.hide.fluids', (event) => {
     [
-      // 'thermal:crude_oil',
+      'thermal:crude_oil',
+      'immersivepetroleum:oil',
+
     ].forEach((disabledFluid) => {
         if (!Fluid.of(disabledFluid).isEmpty()) {
             event.hide(disabledFluid);
