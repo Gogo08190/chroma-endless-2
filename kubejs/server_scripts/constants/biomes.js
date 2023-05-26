eaaf16dcec2544589e973c6d85df4f1c80b629ec
Gogo08 <gogo08190@gmail.com>
2023-05-26 07:23:15 +0200

    [✨] Add biomes list

---
+++ kubejs/server_scripts/constants/biomes.js
@@ -0,0 +1,30 @@
+//priority: 1000
+
+const end_biomes = [
+    'minecraft:the_end',
+    'minecraft:the_void',
+    'minecraft:small_end_islands',
+    'minecraft:end_barrens',
+    'minecraft:end_highlands',
+    'minecraft:end_midlands'
+];
+
+const nether_biomes = [
+    'minecraft:nether_wastes',
+    'minecraft:basalt_deltas',
+    'minecraft:warped_forest',
+    'minecraft:crimson_forest',
+    'minecraft:soul_sand_valley'
+];
+
+const undergarden_biomes = [
+    'undergarden:barren_abyss',
+    'undergarden:dense_forest',
+    'undergarden:forgotten_field',
+    'undergarden:frostfields',
+    'undergarden:gronglegrowth',
+    'undergarden:mushroom_bog',
+    'undergarden:smog_spires',
+    'undergarden:smogstem_forest',
+    'undergarden:wigglewood_forest'
+];
