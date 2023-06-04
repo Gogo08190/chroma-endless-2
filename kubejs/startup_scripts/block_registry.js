57825043677d53935fad121b247e29af0d869189
Gogo08 <gogo08190@gmail.com>
2023-06-04 16:09:23 +0200

    [✨] Nebula extractor part test

--- kubejs/startup_scripts/block_registry.js
+++ kubejs/startup_scripts/block_registry.js
@@ -3,10 +3,19 @@ onEvent('block.registry', (event) => {
       {name: 'andesite_alloy_block', displayName: 'Block of Andesite Alloy', material: 'stone', hardness: '0.4'},
       {name: 'gaia_spirit_block', displayName: 'Block of Gaia Spirit Ingot', material: 'stone', hardness: '0.4'},
       {name: 'dielectric_paste_block', displayName: 'Block of Dielectric Paste', material: 'stone', hardness: '0.4'},
-      {name: 'unassembled_pity_machine_frame', displayName: 'Unassembled Pity Machine Frame', material: 'stone', hardness: '0.4'}
+      {name: 'unassembled_pity_machine_frame', displayName: 'Unassembled Pity Machine Frame', material: 'stone', hardness: '0.4'},
+    ];
+
+    const parts = [
+      {name: 'nebula_extractor_t1_laser_support', displayName: 'Nebula Extractor Laser Support'},
+      {name: 'nebula_extractor_t1_laser_top', displayName: 'Nebula Extractor Laser'}
     ];
 
     blocks.forEach((block) => {
         event.create('chroma:' + block.name).material(block.material).hardness(block.hardness).displayName(block.displayName);
+    });
+
+    parts.forEach((part) => {
+        event.create('chroma:' + part.name).displayName(part.displayName).renderType("cutout").notSolid().lightLevel(1.0);
     });
 });
