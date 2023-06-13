29e3ef440a922f5ef8be6a2c2d7077dc3545639e
Gogo08 <gogo08190@gmail.com>
2023-06-13 07:14:22 +0200

    [✨] Mana diamond plate alternative recipe

---
+++ kubejs/server_scripts/pack/recipetypes/beyond_earth/rolling.js
@@ -0,0 +1,21 @@
+onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:pack/beyond_earth/rolling/';
+    const recipes = [
+        {
+            input: {
+              tag: 'forge:gems/mana_diamond'
+            },
+            cookTime: 200,
+            output: {
+                item: 'chroma:mana_diamond_plate',
+                count: 1
+            },
+            id: `${id_prefix}mana_diamond_plate`
+        },
+    ];
+
+    recipes.forEach((recipe) => {
+        recipe.type = 'beyond_earth_giselle_addon:rolling';
+        event.custom(recipe).id(recipe.id);
+    });
+});
