26b99a3b0269a0f839b5746962e21e4fc51fd50f
YukiiSamaa <thomas.levasseur.tl@gmail.com>
2023-06-15 20:42:23 +0200

    [🍱] Add Raw feranicalumite block + texture

--- kubejs/startup_scripts/block_registry.js
+++ kubejs/startup_scripts/block_registry.js
@@ -7,6 +7,7 @@ onEvent('block.registry', (event) => {
       {name: 'raw_cobalrite_block', displayName: 'Raw Cobalrite Block', material: 'stone', hardness: '0.4'},
       {name: 'raw_aurizincite_block', displayName: 'Raw Aurizincite Block', material: 'stone', hardness: '0.4'},
       {name: 'raw_cuperargentite_block', displayName: 'Raw Cuperargentite Block', material: 'stone', hardness: '0.4'},
+      {name: 'raw_feranicalumite_block', displayName: 'Raw Feranicalumite Block', material: 'stone', hardness: '0.4'},
     ];
 
     const parts = [
