73c9aca5c21db87aa797bdcce1dd481db3aaaa96
Gogo08 <gogo08190@gmail.com>
2023-06-16 07:35:13 +0200

    [✨] Runescribing function

--- kubejs/server_scripts/pack/recipetypes/mana_and_artifice/runescribing.js
+++ kubejs/server_scripts/pack/recipetypes/mana_and_artifice/runescribing.js
@@ -1,10 +1,29 @@
-onEvent('recipes', event => {
+onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:pack/mna/runescribing/';
+    const recipes = [
+      {
+          tier: 1,
+          mutex_h: 17730430967808,
+          mutex_v: 19929856606212,
+          output: 'minecraft:dirt',
+          id: `${id_prefix}runescribing_fire_rune`
+      },
+      {
+          tier: 1,
+          mutex_h: 3448395687530496,
+          mutex_v: 9024802111119360,
+          output: 'minecraft:dirt',
+          id: `${id_prefix}runescribing_water_rune`
+      },
+    ];
 
-    event.custom({
-        "type": "mna:runescribing",
-        "tier": 1,
-        "mutex_h": 8813479461888,
-        "mutex_v": 2251853773415424,
-        "output": "minecraft:dirt"
-    })
-})
+    recipes.forEach((recipe) => {
+        event.custom({
+          type: 'mna:runescribing',
+          output: recipe.output,
+          tier: recipe.tier,
+          mutex_h: recipe.mutex_h,
+          mutex_v: recipe.mutex_v,
+        }).id(recipe.id);
+    });
+});
