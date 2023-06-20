b54f8a0fbe51364d3752f5b9231f19e465aab06b
Gogo08 <gogo08190@gmail.com>
2023-06-20 11:41:56 +0200

    [:sparkles:] Rftools recipe (extended)

--- kubejs/server_scripts/pack/recipetypes/extendedcrafting/advanced_table.js
+++ kubejs/server_scripts/pack/recipetypes/extendedcrafting/advanced_table.js
@@ -39,4 +39,166 @@ onEvent("recipes", (event) => {
       }
     }
   ).id('ae2:network/blocks/controller')
+
+  event.custom(
+    {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "ABCBA",
+        "BDEDB",
+        "FEGEF",
+        "BDEDB",
+        "ABHBA"
+      ],
+      "key": {
+        "A": {
+          "item": "powah:capacitor_hardened"
+        },
+        "B": {
+          "item": "pneumaticcraft:compressed_bricks"
+        },
+        "C": {
+          "type": "forge:nbt",
+          "item": "thermal:flux_drill",
+          "count": 1,
+          "nbt": "{Energy:28080}"
+        },
+        "D": {
+          "item": "mekanism:alloy_reinforced"
+        },
+        "E": {
+          "item": "mekanism:elite_control_circuit"
+        },
+        "F": {
+          "item": "pneumaticcraft:transistor"
+        },
+        "G": {
+          "item": "rftoolsbase:machine_frame"
+        },
+        "H": {
+          "type": "forge:nbt",
+          "item": "thermal:flux_saw",
+          "count": 1,
+          "nbt": "{Energy:33000}"
+        }
+      },
+      "result": {
+        "item": "rftoolsbuilder:builder"
+      }
+    }
+  ).id('rftoolsbuilder:builder')
+
+  event.custom(
+    {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "ABCDA",
+        "EFGFH",
+        "IGJGI",
+        "KFGFL",
+        "AMNOA"
+      ],
+      "key": {
+        "A": {
+          "item": "pneumaticcraft:ingot_iron_compressed"
+        },
+        "B": {
+          "item": "minecraft:bone"
+        },
+        "C": {
+          "item": "pneumaticcraft:spawner_extractor"
+        },
+        "D": {
+          "item": "minecraft:ghast_tear"
+        },
+        "E": {
+          "item": "minecraft:rotten_flesh"
+        },
+        "F": {
+          "item": "rftoolsbase:infused_diamond"
+        },
+        "G": {
+          "item": "mekanism:elite_control_circuit"
+        },
+        "H": {
+          "item": "minecraft:blaze_rod"
+        },
+        "I": {
+          "item": "rftoolsbase:infused_enderpearl"
+        },
+        "J": {
+          "item": "rftoolsbase:machine_frame"
+        },
+        "K": {
+          "item": "minecraft:shulker_shell"
+        },
+        "L": {
+          "item": "minecraft:wither_skeleton_skull"
+        },
+        "M": {
+          "item": "minecraft:ender_pearl"
+        },
+        "N": {
+          "item": "solarpanels:improved_energy_tablet"
+        },
+        "O": {
+          "item": "minecraft:phantom_membrane"
+        }
+      },
+      "result": {
+        "item": "rftoolsutility:spawner"
+      }
+    }
+  ).id('rftoolsutility:spawner')
+
+  event.custom(
+    {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "ABCBA",
+        "BDEDB",
+        "CFGHC",
+        "IDJDI",
+        "AIKIA"
+      ],
+      "key": {
+        "A": {
+          "item": "mna:decoration/transmuted_silver_block"
+        },
+        "B": {
+          "item": "mna:decoration/vinteum_arcane_stone"
+        },
+        "C": {
+          "item": "rftoolsbase:infused_diamond"
+        },
+        "D": {
+          "item": "botania:gaia_ingot"
+        },
+        "E": {
+          "item": "botania:rune_air"
+        },
+        "F": {
+          "item": "botania:rune_fire"
+        },
+        "G": {
+          "item": "rftoolsbase:machine_frame"
+        },
+        "H": {
+          "item": "botania:rune_earth"
+        },
+        "I": {
+          "item": "mna:decoration/redstone_arcane_stone"
+        },
+        "J": {
+          "item": "botania:rune_water"
+        },
+        "K": {
+          "item": "solarpanels:improved_energy_tablet"
+        }
+      },
+      "result": {
+        "item": "rftoolsutility:environmental_controller"
+      }
+    }
+  ).id('rftoolsutility:environmental_controller')
 });
