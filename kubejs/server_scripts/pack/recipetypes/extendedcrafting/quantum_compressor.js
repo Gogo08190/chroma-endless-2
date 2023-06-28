3544be63c72d64a11fe1f440b81da83b4c59142d
Gogo08 <gogo08190@gmail.com>
2023-06-28 18:05:27 +0200

    [✨] Quantum compressor script

--- kubejs/server_scripts/pack/recipetypes/extendedcrafting/quantum_compressor.js
+++ kubejs/server_scripts/pack/recipetypes/extendedcrafting/quantum_compressor.js
@@ -1,20 +1,26 @@
 onEvent('recipes', (event) => {
     const id_prefix = 'chroma:pack/extendedcrafting/quantum_compressor/';
-    
-    event.custom(
-        {
-            "type": "extendedcrafting:compressor",
-            "powerCost": 500000,
-            "inputCount": 256,
-            "ingredient": {
-              "item": "undergarden:utherium_crystal"
-            },
-            "catalyst": {
-              "item": "mekanism:teleportation_core"
-            },
-            "result": {
-              "item": "chroma:miner_core"
-            }
-          }
-    ).id(`${id_prefix}miner_core`)
+
+    const recipes = [
+      {
+          powerCost: 500000,
+          inputCount: 256,
+          ingredient: { item: 'undergarden:utherium_crystal' },
+          result: { item: 'chroma:miner_core' },
+          id: `${id_prefix}miner_core`
+      },
+    ];
+
+    recipes.forEach((recipe) => {
+        event
+            .custom({
+                type: 'extendedcrafting:compressor',
+                powerCost: recipe.powerCost,
+                inputCount: recipe.inputCount,
+                ingredient: recipe.ingredient,
+                catalyst: recipe.catalyst,
+                result: recipe.result
+            })
+            .id(recipe.id);
+    });
 });
