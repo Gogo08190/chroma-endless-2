8ea90e4a076267942954061912959115f0a6a023
Gogo08 <gogo08190@gmail.com>
2023-06-28 18:07:18 +0200

    [✨] Remove other obsidian dust recipes

--- kubejs/server_scripts/pack/recipetypes/mekanism/remove.js
+++ kubejs/server_scripts/pack/recipetypes/mekanism/remove.js
@@ -1,6 +1,12 @@
 onEvent('recipes', (event) => {
     const idRemovals = [
-        'mekanism:structural_glass'
+        'mekanism:structural_glass',
+        'morevanillalib:obsidian_shard_decompress',
+        'jaopca:immersiveengineering.material_to_dust.obsidian',
+        'lazierae2:compat/mekanism/grinder/dust_obsidian',
+        'jaopca:mekanism.material_to_dust.obsidian',
+        'mekanism:enriching/conversion/obsidian_to_obsidian_dust',
+        'jaopca:thermal_expansion.material_to_dust.obsidian'
     ];
 
     const outputRemovals = [
