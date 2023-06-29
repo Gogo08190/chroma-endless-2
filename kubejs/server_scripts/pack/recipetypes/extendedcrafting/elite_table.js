e89f28346867caa2b8e9327c976561d625e20f87
koepss <pierrelode2@outlook.fr>
2023-06-29 18:28:31 +0200

    [:fire:]add craft domm armor

--- kubejs/server_scripts/pack/recipetypes/extendedcrafting/elite_table.js
+++ kubejs/server_scripts/pack/recipetypes/extendedcrafting/elite_table.js
@@ -71,4 +71,286 @@ onEvent("recipes", (event) => {
             }
           }
     ).id('mekanism:digital_miner')
+
+    event.custom(
+            {
+          "type": "extendedcrafting:shaped_table",
+          "pattern": [
+            "AAAAAAA",
+            "ABCDCBA",
+            "AECFCEA",
+            "AGCCCGA",
+            "AHIIIHA",
+            "JKILIKJ",
+            "JJIMIJJ"
+          ],
+          "key": {
+            "A": {
+              "item": "doom:argent_energy"
+            },
+            "B": {
+              "type": "forge:nbt",
+              "item": "doom:doom_helmet",
+              "count": 1,
+              "nbt": "{Damage:0}"
+            },
+            "C": {
+              "item": "biggerreactors:blutonium_block"
+            },
+            "D": {
+              "type": "forge:nbt",
+              "item": "immersiveengineering:revolver",
+              "count": 1,
+              "nbt": "{bullets:{Items:[]}}"
+            },
+            "E": {
+              "type": "forge:nbt",
+              "item": "doom:doom_chestplate",
+              "count": 1,
+              "nbt": "{Damage:0}"
+            },
+            "F": {
+              "item": "doom:rocket"
+            },
+            "G": {
+              "type": "forge:nbt",
+              "item": "doom:doom_leggings",
+              "count": 1,
+              "nbt": "{Damage:0}"
+            },
+            "H": {
+              "type": "forge:nbt",
+              "item": "doom:doom_boots",
+              "count": 1,
+              "nbt": "{Damage:0}"
+            },
+            "I": {
+              "item": "doom:argent_plate"
+            },
+            "J": {
+              "item": "mekanism:induction_casing"
+            },
+            "K": {
+              "item": "mekanism:teleportation_core"
+            },
+            "L": {
+              "item": "chroma:gun_table_heart"
+            },
+            "M": {
+              "item": "mekaevolution:absolute_control_circuit"
+            }
+          },
+          "result": {
+            "item": "doom:gun_table"
+          }
+        }
+      ).id('doom:gun_table')
+
+    event.custom(
+        {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "  A A  ",
+        " AA AA ",
+        " AA AA ",
+        " AB BA ",
+        " AB BA ",
+        " AB BA ",
+        "BBB BBB"
+      ],
+      "key": {
+        "A": {
+          "item": "doom:argent_energy"
+        },
+        "B": {
+          "item": "doom:argent_plate"
+        }
+      },
+      "result": {
+        "item": "doom:doom_boots"
+      }
+    }
+  ).id('doom:doom_boots')
+
+  event.custom(
+    {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "AAAAAAA",
+        "AAAAAAA",
+        "AABBBAA",
+        "AAB BAA",
+        "AAB BAA",
+        "AAB BAA",
+        "AAB BAA"
+      ],
+      "key": {
+        "A": {
+          "item": "doom:argent_plate"
+        },
+        "B": {
+          "item": "doom:argent_energy"
+        }
+      },
+      "result": {
+        "item": "doom:doom_leggings"
+      }
+    }
+  ).id('doom:doom_leggings')
+
+  event.custom(
+    {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "AABBBAA",
+        "AABBBAA",
+        " ABBBA ",
+        " ABBBA ",
+        " ABBBA ",
+        " ABBBA ",
+        " AAAAA "
+      ],
+      "key": {
+        "A": {
+          "item": "doom:argent_plate"
+        },
+        "B": {
+          "item": "doom:argent_energy"
+        }
+      },
+      "result": {
+        "item": "doom:doom_chestplate"
+      }
+    }
+  ).id('doom:doom_chestplate')
+
+    event.custom(
+      {
+        "type": "extendedcrafting:shaped_table",
+        "pattern": [
+          "AAAAAAA",
+          "ABBBBBA",
+          "ABBBBBA",
+          "AB   BA",
+          "       ",
+          "       ",
+          "       "
+        ],
+        "key": {
+          "A": {
+            "item": "doom:argent_plate"
+          },
+          "B": {
+            "item": "doom:argent_energy"
+          }
+        },
+        "result": {
+          "item": "doom:doom_helmet"
+        }
+      }
+    ).id('doom:doom_helmet')
+
+      event.custom(
+        {
+        "type": "extendedcrafting:shaped_table",
+        "pattern": [
+          "  A A  ",
+          " AA AA ",
+          " AA AA ",
+          " AB BA ",
+          " AB BA ",
+          " AB BA ",
+          "BBB BBB"
+        ],
+        "key": {
+          "A": {
+            "item": "doom:argent_plate"
+          },
+          "B": {
+            "item": "doom:argent_energy"
+          }
+        },
+        "result": {
+          "item": "doom:praetor_doom_boots"
+        }
+      }
+    ).id('doom:praetor_doom_boots')
+
+    event.custom(
+      {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "AAAAAAA",
+        "AAAAAAA",
+        "AABBBAA",
+        "AAB BAA",
+        "AAB BAA",
+        "AAB BAA",
+        "AAB BAA"
+      ],
+      "key": {
+        "A": {
+          "item": "doom:argent_energy"
+        },
+        "B": {
+          "item": "doom:argent_plate"
+        }
+      },
+      "result": {
+        "item": "doom:praetor_doom_leggings"
+      }
+    }
+  ).id('doom:praetor_doom_leggings')
+
+  event.custom(
+    {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "AAB BAA",
+        "AABBBAA",
+        " ABBBA ",
+        " ABBBA ",
+        " ABBBA ",
+        " ABBBA ",
+        " AAAAA "
+      ],
+      "key": {
+        "A": {
+          "item": "doom:argent_energy"
+        },
+        "B": {
+          "item": "doom:argent_plate"
+        }
+      },
+      "result": {
+        "item": "doom:praetor_doom_chestplate"
+      }
+    }
+  ).id('doom:praetor_doom_chestplate')
+
+  event.custom(
+    {
+      "type": "extendedcrafting:shaped_table",
+      "pattern": [
+        "AAAAAAA",
+        "ABBBBBA",
+        "ABBBBBA",
+        "AB   BA",
+        "       ",
+        "       ",
+        "       "
+      ],
+      "key": {
+        "A": {
+          "item": "doom:argent_energy"
+        },
+        "B": {
+          "item": "doom:argent_plate"
+        }
+      },
+      "result": {
+        "item": "doom:praetor_doom_helmet"
+      }
+    }
+  ).id('doom:praetor_doom_helmet')
   });
