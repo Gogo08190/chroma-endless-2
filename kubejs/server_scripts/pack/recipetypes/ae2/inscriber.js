fa9d164b0d6e3f8164fa6f7e37e80ed17763d93c
Gogo08 <gogo08190@gmail.com>
2023-06-29 11:38:42 +0200

    [:sparkles:] AE2 + Lazier inscriber scripts

---
+++ kubejs/server_scripts/pack/recipetypes/ae2/inscriber.js
@@ -0,0 +1,29 @@
+onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:pack/ae2/inscriber/';
+    const recipes = [
+      {
+        mode: 'press',
+        top: { item: 'lazierae2:parallel_printed' },
+        middle: { item: 'extendedcrafting:redstone_ingot' },
+        bottom: { item: 'ae2:printed_silicon' },
+        result: { item: 'lazierae2:parallel_processor' },
+        id: 'lazierae2:compat/ae2/inscriber/parallel_processor'
+      },
+    ];
+
+
+    recipes.forEach((recipe) => {
+        event
+            .custom({
+                type: 'ae2:inscriber',
+                mode: recipe.mode,
+                ingredients: {
+                  middle: recipe.middle,
+                  top: recipe.top,
+                  bottom: recipe.bottom
+                },
+                result: recipe.result
+            })
+            .id(recipe.id);
+    });
+});
---
+++ kubejs/server_scripts/pack/recipetypes/lazierae2/etcher.js
@@ -0,0 +1,32 @@
+onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:pack/lazierae2/etcher/';
+    const recipes = [
+      {
+        output: { item: 'lazierae2:parallel_processor' },
+        inputs: [
+          { tag: 'forge:gems/resonating' },
+          { item: 'extendedcrafting:redstone_ingot' },
+          { tag: 'forge:silicon' }
+        ],
+        process_time: 120,
+        energy_cost: 1500,
+        id: 'lazierae2:etcher/parallel_processor'
+      },
+    ];
+
+    recipes.forEach((recipe) => {
+        let ingredients = [];
+
+        recipe.inputs.forEach((input) => {
+            ingredients.push(Ingredient.of(input));
+        });
+
+        event.custom({
+            type: 'lazierae2:etcher',
+            output: recipe.output,
+            input: ingredients,
+            process_time: recipe.process_time,
+            energy_cost: recipe.energy_cost,
+        }).id(recipe.id);
+    });
+});
