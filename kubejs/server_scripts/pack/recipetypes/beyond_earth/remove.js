7b63533d0ddab6b45eda4fd79f8697a4601b9ecd
Gogo08 <gogo08190@gmail.com>
2023-07-01 09:31:23 +0200

    [✨] Remove desh plate from hammer

--- kubejs/server_scripts/pack/recipetypes/beyond_earth/remove.js
+++ kubejs/server_scripts/pack/recipetypes/beyond_earth/remove.js
@@ -3,6 +3,7 @@
 onEvent('recipes', (event) => {
     const idRemovals = [
       'beyond_earth:steel_block',
+      'jaopca:immersiveengineering.material_to_plate_hammer.desh'
     ];
 
     const outputRemovals = [
