717a6f7d4542dbe5ed0da67c8a65f72281a81576
Gogo08 <gogo08190@gmail.com>
2023-07-03 18:36:58 +0200

    [✨] Remove andesite alloy block

--- kubejs/startup_scripts/block_registry.js
+++ kubejs/startup_scripts/block_registry.js
@@ -1,6 +1,5 @@
 onEvent('block.registry', (event) => {
     const blocks = [
-      {name: 'andesite_alloy_block', displayName: 'Block of Andesite Alloy', material: 'stone', hardness: '0.4'},
       {name: 'gaia_spirit_block', displayName: '§bBlock of Gaia Spirit Ingot', material: 'stone', hardness: '0.4'},
       {name: 'dielectric_paste_block', displayName: 'Block of Dielectric Paste', material: 'stone', hardness: '0.4'},
       {name: 'unassembled_pity_machine_frame', displayName: '§aUnassembled Pity Machine Frame', material: 'stone', hardness: '0.4'},
