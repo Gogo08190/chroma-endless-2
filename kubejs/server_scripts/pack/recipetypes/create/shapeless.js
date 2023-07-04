02a7f553b03351884b88d9f42eefe43a83528c51
Gogo08 <gogo08190@gmail.com>
2023-07-04 07:36:00 +0200

    [✨] Update scripts before create updating

--- kubejs/server_scripts/pack/recipetypes/create/shapeless.js
+++ kubejs/server_scripts/pack/recipetypes/create/shapeless.js
@@ -14,20 +14,6 @@ onEvent('recipes', (event) => {
             inputs: ['4x create:andesite_alloy'],
             id: 'createdeco:andesite_trapdoor'
         },
-        {
-            output: 'chroma:andesite_alloy_block',
-            inputs: [
-                '9x create:andesite_alloy'
-            ],
-            id: 'chroma:block_from_ingot/andesite_alloy'
-        },
-        {
-            output: Item.of('create:andesite_alloy', 9),
-            inputs: [
-                'chroma:andesite_alloy_block'
-            ],
-            id: 'chroma:ingot_from_block/andesite_alloy'
-        },
     ];
 
     recipes.forEach((recipe) => {
--- kubejs/server_scripts/pack/recipetypes/tags/chroma/items.js
+++ kubejs/server_scripts/pack/recipetypes/tags/chroma/items.js
@@ -10,7 +10,6 @@ const TAGS_ITEMS = [
 
     ["forge:storage_blocks/mana_diamond", "botania:mana_diamond_block"],
     ["forge:storage_blocks/dragonstone", "botania:dragonstone_block"],
-    ["forge:storage_blocks/andesite_alloy", "chroma:andesite_alloy_block"],
 
     ["forge:rods/electrum", "chroma:electrum_rod"],
     ["forge:rods/manasteel", "chroma:manasteel_rod"],
