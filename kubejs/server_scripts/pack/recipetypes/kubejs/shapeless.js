b25002f4148b1f47803a8677d87dab377fc4bf8b
Gogo08 <gogo08190@gmail.com>
2023-07-09 13:10:15 +0200

    [:bug:] Fix recipe id Black bone meal

--- kubejs/server_scripts/pack/recipetypes/kubejs/shapeless.js
+++ kubejs/server_scripts/pack/recipetypes/kubejs/shapeless.js
@@ -1,9 +1,10 @@
 onEvent('recipes', (event) => {
+    const id_prefix = 'chroma:pack/kubejs/shapeless/';
     const recipes = [
         {
             output: '3x chroma:black_bone_meal',
             inputs: ['architects_palette:withered_bone'],
-            id: 'architects_palette:bone_meal'
+            id: `${id_prefix}black_bone_meal`
         },
     ];
 
