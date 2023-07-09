ffd397696987a1df8044d0cb8993635b8c38d8f1
Gogo08 <gogo08190@gmail.com>
2023-07-09 13:19:12 +0200

    [✨] Remove alloys recipes from crafting table

---
+++ kubejs/server_scripts/pack/recipetypes/thermal/remove.js
@@ -0,0 +1,30 @@
+onEvent('recipes', (event) => {
+    const idRemovals = [
+      'thermal:fire_charge/lumium_ingot_4',
+      'thermal:lumium_dust_4',
+      'thermal:fire_charge/enderium_ingot_2',
+      'thermal:enderium_dust_2',
+      'thermal:fire_charge/signalum_ingot_4',
+      'thermal:signalum_dust_4',
+      'thermal:fire_charge/electrum_ingot_2',
+      'immersiveengineering:crafting/electrum_mix',
+      'thermal:fire_charge/constantan_ingot_2',
+      'immersiveengineering:crafting/constantan_mix',
+      'thermal:fire_charge/invar_ingot_3',
+      'thermal:invar_dust_3',
+      'thermal:fire_charge/bronze_ingot_4',
+      'thermal:bronze_dust_4',
+    ];
+
+    const outputRemovals = [
+
+    ];
+
+    idRemovals.forEach((id) => {
+        event.remove({ id: id });
+    });
+
+    outputRemovals.forEach((output) => {
+        event.remove({ output: output });
+    });
+});
