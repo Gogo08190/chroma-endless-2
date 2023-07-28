38974c081fd184448976096a8d8dd220c5d5b869
Gogo08 <gogo08190@gmail.com>
2023-07-28 11:08:12 +0200

    [:sparkles:] Remove mythic botany rune default recipes

--- kubejs/server_scripts/pack/recipetypes/botania/remove.js
+++ kubejs/server_scripts/pack/recipetypes/botania/remove.js
@@ -4,6 +4,15 @@ onEvent('recipes', (event) => {
       'botanicalmachinery:mana_tablet',
       'botanicalmachinery:runic_altar',
       'botania:diluted_pool',
+      'mythicbotany:nidavellir_rune_runic_altar',
+      'mythicbotany:niflheim_rune_runic_altar',
+      'mythicbotany:muspelheim_rune_runic_altar',
+      'mythicbotany:joetunheim_rune_runic_altar',
+      'mythicbotany:midgard_rune_runic_altar',
+      'mythicbotany:alfheim_rune_runic_altar',
+      'mythicbotany:vanaheim_rune_runic_altar',
+      'mythicbotany:asgard_rune_runic_altar',
+      'mythicbotany:helheim_rune_runic_altar'
     ];
 
     const outputRemovals = [
