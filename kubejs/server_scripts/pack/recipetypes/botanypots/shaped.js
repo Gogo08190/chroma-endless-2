6cbcb13c58b14ca4164591ec4cb739607faab741
Gogo08 <gogo08190@gmail.com>
2023-08-02 08:20:03 +0200

    [:zap:] Improved botanypots script

--- kubejs/server_scripts/pack/recipetypes/botanypots/shaped.js
+++ kubejs/server_scripts/pack/recipetypes/botanypots/shaped.js
@@ -1,150 +1,40 @@
 onEvent('recipes', (event) => {
-    const recipes = [
-        {
-            output: 'botanypots:terracotta_botany_pot',
-            pattern: ['T T', 'TPT', ' T '],
-            key: {
-                T: 'minecraft:terracotta',
-                P: 'supplementaries:planter'
-            },
-            id: 'botanypots:botanypots/crafting/terracotta_botany_pot'
-        },
-        {
-            output: 'botanypots:terracotta_hopper_botany_pot',
-            pattern: ['THT', 'TPT', ' T '],
-            key: {
-                T: 'minecraft:terracotta',
-                P: 'supplementaries:planter',
-                H: 'create:chute'
-            },
-            id: 'botanypots:botanypots/crafting/terracotta_compact_hopper_botany_pot'
-        },
-        {
-            output: 'botanypots:terracotta_hopper_botany_pot',
-            pattern: ['P', 'C'],
-            key: {
-                P: 'botanypots:terracotta_botany_pot',
-                C: 'create:chute'
-            },
-            id: 'botanypots:botanypots/crafting/terracotta_hopper_botany_pot'
-        }
+    const colors = ['white', 'orange', 'magenta', 'light_blue', 'yellow', 'lime', 'pink', 'gray', 'light_gray', 'cyan', 'purple', 'blue', 'brown', 'green', 'red', 'black'];
+    const types = ['terracotta', 'concrete', 'glazed_terracotta'];
+    const planter = 'supplementaries:planter';
+    const chute = 'create:chute';
+
+    const createRecipe = (output, pattern, key, id) => {
+      return {
+          "output": output,
+          "pattern": pattern,
+          "key": key,
+          "id": id
+      };
+    }
+
+
+    let recipes = [
+        createRecipe('botanypots:terracotta_botany_pot', ['T T', 'TPT', ' T '], {T: 'minecraft:terracotta', P: planter}, 'botanypots:botanypots/crafting/terracotta_botany_pot'),
+        createRecipe('botanypots:terracotta_hopper_botany_pot', ['THT', 'TPT', ' T '], {T: 'minecraft:terracotta', P: planter, H: chute}, 'botanypots:botanypots/crafting/terracotta_compact_hopper_botany_pot'),
+        createRecipe('botanypots:terracotta_hopper_botany_pot', ['P', 'C'], {P: 'botanypots:terracotta_botany_pot', C: chute}, 'botanypots:botanypots/crafting/terracotta_hopper_botany_pot')
     ];
 
-    let coloredPots = [
-      'white',
-      'orange',
-      'magenta',
-      'light_blue',
-      'yellow',
-      'lime',
-      'pink',
-      'gray',
-      'light_gray',
-      'cyan',
-      'purple',
-      'blue',
-      'brown',
-      'green',
-      'red',
-      'black'
-    ];
-
-    coloredPots.forEach((coloredPot) => {
-      recipes.push(
-        // Terracotta
-        {
-          output: `botanypots:${coloredPot}_terracotta_botany_pot`,
-          pattern: ['T T', 'TPT', ' T '],
-          key: {
-              T: `minecraft:${coloredPot}_terracotta`,
-              P: 'supplementaries:planter'
-          },
-          id: `botanypots:botanypots/crafting/${coloredPot}_terracotta_botany_pot`
-        },
-        {
-          output: `botanypots:${coloredPot}_terracotta_hopper_botany_pot`,
-          pattern: ['THT', 'TPT', ' T '],
-          key: {
-              T: `minecraft:${coloredPot}_terracotta`,
-              P: 'supplementaries:planter',
-              H: 'create:chute'
-          },
-          id: `botanypots:botanypots/crafting/${coloredPot}_terracotta_compact_hopper_botany_pot`
-        },
-        {
-          output: `botanypots:${coloredPot}_terracotta_hopper_botany_pot`,
-          pattern: ['T', 'H'],
-          key: {
-              T: `botanypots:${coloredPot}_terracotta_botany_pot`,
-              H: 'create:chute'
-          },
-          id: `botanypots:botanypots/crafting/${coloredPot}_terracotta_hopper_botany_pot`
-        },
-
-        // Concrete
-        {
-          output: `botanypots:${coloredPot}_concrete_botany_pot`,
-          pattern: ['T T', 'TPT', ' T '],
-          key: {
-              T: `minecraft:${coloredPot}_concrete`,
-              P: 'supplementaries:planter'
-          },
-          id: `botanypots:botanypots/crafting/${coloredPot}_concrete_botany_pot`
-        },
-        {
-          output: `botanypots:${coloredPot}_concrete_hopper_botany_pot`,
-          pattern: ['THT', 'TPT', ' T '],
-          key: {
-              T: `minecraft:${coloredPot}_concrete`,
-              P: 'supplementaries:planter',
-              H: 'create:chute'
-          },
-          id: `botanypots:botanypots/crafting/${coloredPot}_concrete_compact_hopper_botany_pot`
-        },
-        {
-          output: `botanypots:${coloredPot}_concrete_hopper_botany_pot`,
-          pattern: ['T', 'H'],
-          key: {
-              T: `botanypots:${coloredPot}_concrete_botany_pot`,
-              H: 'create:chute'
-          },
-          id: `botanypots:botanypots/crafting/${coloredPot}_concrete_hopper_botany_pot`
-        },
-
-        // Glazed Terracotta
-        {
-          output: `botanypots:${coloredPot}_glazed_terracotta_botany_pot`,
-          pattern: ['T T', 'TPT', ' T '],
-          key: {
-              T: `minecraft:${coloredPot}_glazed_terracotta`,
-              P: 'supplementaries:planter'
-          },
-          id: `botanypots:botanypots/crafting/${coloredPot}_glazed_terracotta_botany_pot`
-        },
-        {
-          output: `botanypots:${coloredPot}_glazed_terracotta_hopper_botany_pot`,
-          pattern: ['THT', 'TPT', ' T '],
-          key: {
-              T: `minecraft:${coloredPot}_glazed_terracotta`,
-              P: 'supplementaries:planter',
-              H: 'create:chute'
-          },
-          id: `botanypots:botanypots/crafting/${coloredPot}_glazed_terracotta_compact_hopper_botany_pot`
-        },
-        {
-          output: `botanypots:${coloredPot}_glazed_terracotta_hopper_botany_pot`,
-          pattern: ['T', 'H'],
-          key: {
-              T: `botanypots:${coloredPot}_glazed_terracotta_botany_pot`,
-              H: 'create:chute'
-          },
-          id: `botanypots:botanypots/crafting/${coloredPot}_glazed_terracotta_hopper_botany_pot`
-        }
-      );
+    colors.forEach(color => {
+        types.forEach(type => {
+            const idPrefix = `botanypots:botanypots/crafting/${color}_${type}`;
+            const outputPrefixHopper = `botanypots:${color}_${type}_hopper_botany_pot`;
+            const outputPrefix = `botanypots:${color}_${type}_botany_pot`;
+            const minecraftType = `minecraft:${color}_${type}`;
+            recipes.push(
+                createRecipe(`${outputPrefix}`, ['T T', 'TPT', ' T '], {T: minecraftType, P: planter}, `${idPrefix}_botany_pot`),
+                createRecipe(`${outputPrefixHopper}`, ['THT', 'TPT', ' T '], {T: minecraftType, P: planter, H: chute}, `${idPrefix}_compact_hopper_botany_pot`),
+                createRecipe(`${outputPrefixHopper}`, ['T', 'H'], {T: `${outputPrefix}`, H: chute}, `${idPrefix}_hopper_botany_pot`)
+            );
+        });
     });
 
-
-    recipes.forEach((recipe) => {
+    recipes.forEach(recipe => {
         event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
     });
 });
