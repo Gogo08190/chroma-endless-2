f719dc667c0eb9dd96e92cc8117d3196cbad0b8a
MysterBoy13 <laurendeauflorian@gmail.com>
2023-08-05 16:41:24 +0200

    :sparkles: Wormhole part1

--- kubejs/server_scripts/wormhole/shaped.js
+++ kubejs/server_scripts/wormhole/shaped.js
@@ -22,7 +22,51 @@ onEvent('recipes', (event) => {
             },
             id: 'wormhole:portal_frame'
         },
+        {
+            output: 'wormhole:target_device',
+            pattern: ['  R', 'IBI', 'IPI'],
+            key: {
+                R: '#forge:dusts/redstone',
+                I: '#forge:plates/iron',
+                B: Item.of('evilcraft:blood_pearl_of_teleportation').ignoreNBT(),
+                P: 'compactmachines:personal_shrinking_device'
+            },
+            id: 'wormhole:target_device'
+        },
+        {
+            output: 'wormhole:coal_generator',
+            pattern: ['CEC', 'CBC', 'CHC'],
+            key: {
+                C: '#forge:ingots/compressed_iron',
+                E: '#forge:ender_pearls',
+                B: 'ironfurnaces:copper_furnace',
+                H: 'immersiveengineering:furnace_heater'
+            },
+            id: 'wormhole:coal_generator'
+        },
+        {
+            output: 'wormhole:advanced_target_cell',
+            pattern: ['CPC', 'VBV', 'CPC'],
+            key: {
+                C: '#forge:ingots/compressed_iron',
+                P: 'pneumaticcraft:capacitor',
+                B: 'wormhole:basic_target_cell',
+                V: 'mna:superheated_vinteum_ingot'
+            },
+            id: 'wormhole:advanced_target_cell'
+        },
+        {
+            output: 'wormhole:basic_target_cell',
+            pattern: ['CMC', 'MOM', 'CMC'],
+            key: {
+                C: '#forge:ingots/compressed_iron',
+                O: Item.of('minecraft:compass').ignoreNBT(),
+                M: '#forge:ingots/manasteel'
+            },
+            id: 'wormhole:basic_target_cell'
+        },
     ];
+    
 
     recipes.forEach((recipe) => {
         event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
