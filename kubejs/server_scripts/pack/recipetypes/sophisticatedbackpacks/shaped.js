02ea8cdbd6e74e0c22512fc52894b786eff02eac
MysterBoy13 <laurendeauflorian@gmail.com>
2023-08-08 17:59:24 +0200

    :sparkles: sophisticatedbackpacks Part1

--- kubejs/server_scripts/pack/recipetypes/sophisticatedbackpacks/shaped.js
+++ kubejs/server_scripts/pack/recipetypes/sophisticatedbackpacks/shaped.js
@@ -11,6 +11,95 @@ onEvent('recipes', (event) => {
             },
             id: 'sophisticatedbackpacks:upgrade_base'
         },
+        {
+            output: 'functionalstorage:storage_controller',
+            pattern: ['SQS', 'DED', 'SQS'],
+            key: {
+                S: '#forge:stone',
+                Q: '#forge:storage_blocks/quartz',
+                E: 'create:electron_tube',              
+                D: '#functionalstorage:drawer'
+            },
+            id: 'functionalstorage:storage_controller'
+        },
+        {
+            output: 'sophisticatedbackpacks:void_upgrade',
+            pattern: [' E ', 'OBO', 'ROR'],
+            key: {
+                B: 'sophisticatedbackpacks:upgrade_base',
+                E: 'rftoolsbase:infused_enderpearl',
+                R: '#forge:dusts/redstone',              
+                O: 'forbidden_arcanus:processed_obsidian_block'
+            },
+            id: 'sophisticatedbackpacks:void_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:compacting_upgrade',
+            pattern: ['IPI', 'PBP', 'RPR'],
+            key: {
+                B: 'sophisticatedbackpacks:upgrade_base',
+                I: '#forge:plates/iron',
+                R: '#forge:dusts/redstone',              
+                P: 'minecraft:piston'
+            },
+            id: 'sophisticatedbackpacks:compacting_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:compacting_upgrade',
+            pattern: ['RIR', 'IBI', 'RFR'],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                I: '#forge:plates/iron',
+                R: '#forge:dusts/redstone',              
+                F: 'minecraft:furnace'
+            },
+            id: 'sophisticatedbackpacks:smelting_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:smoking_upgrade',
+            pattern: ['RIR', 'IBI', 'RSR'],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                I: '#forge:plates/iron',
+                R: '#forge:dusts/redstone',              
+                S: 'minecraft:smoker'
+            },
+            id: 'sophisticatedbackpacks:smoking_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:blasting_upgrade',
+            pattern: ['RIR', 'IBI', 'RFR'],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                I: '#forge:plates/iron',
+                R: '#forge:dusts/redstone',              
+                F: 'minecraft:blast_furnace'
+            },
+            id: 'sophisticatedbackpacks:blasting_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:filter_upgrade',
+            pattern: ['RSR', 'SBS', 'RSR'],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                R: '#forge:dusts/redstone',              
+                S: 'botania:mana_string'
+            },
+            id: 'sophisticatedbackpacks:filter_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:deposit_upgrade',
+            pattern: [' P ', 'IBI', 'RCR'],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                C: '#forge:chests/wooden',
+                P: 'minecraft:piston',
+                R: '#forge:dusts/redstone',              
+                I: '#forge:plates/iron'
+            },
+            id: 'sophisticatedbackpacks:deposit_upgrade'
+        },
+        
     ];
 
     recipes.forEach((recipe) => {
