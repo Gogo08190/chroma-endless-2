32241625ad6362678efe4760b8a424b4d3b73e2e
MysterBoy13 <laurendeauflorian@gmail.com>
2023-08-11 16:29:13 +0200

    :sparkles: sophisticatedbackpacks Part 2

--- kubejs/server_scripts/pack/recipetypes/sophisticatedbackpacks/shaped.js
+++ kubejs/server_scripts/pack/recipetypes/sophisticatedbackpacks/shaped.js
@@ -88,6 +88,153 @@ onEvent('recipes', (event) => {
             },
             id: 'sophisticatedbackpacks:deposit_upgrade'
         },
+        {
+            output: 'sophisticatedbackpacks:stonecutter_upgrade',
+            pattern: [' C ', 'IBI', ' R '],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                C: 'minecraft:stonecutter',
+                R: '#forge:dusts/redstone',              
+                I: '#forge:plates/iron'
+            },
+            id: 'sophisticatedbackpacks:stonecutter_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:tool_swapper_upgrade',
+            pattern: ['RWR', 'PBA', 'ISI'],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                A: Item.of('minecraft:wooden_axe', '{Damage:0}'),
+                S: Item.of('minecraft:wooden_shovel', '{Damage:0}'),
+                P: Item.of('minecraft:wooden_pickaxe', '{Damage:0}'),
+                W: Item.of('minecraft:wooden_sword', '{Damage:0}'),
+                R: '#forge:dusts/redstone',              
+                I: '#forge:plates/iron'
+            },
+            id: 'sophisticatedbackpacks:tool_swapper_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:jukebox_upgrade',
+            pattern: [' J ', 'IBI', ' R '],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                J: 'minecraft:jukebox',
+                I: '#forge:plates/iron',
+                R: '#forge:dusts/redstone'
+            },
+            id: 'sophisticatedbackpacks:jukebox_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:everlasting_upgrade',
+            pattern: ['CSC', 'SBS', 'CSC'],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                C: 'minecraft:end_crystal',
+                S: 'forbidden_arcanus:dark_nether_star'
+            },
+            id: 'sophisticatedbackpacks:everlasting_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:inception_upgrade',
+            pattern: ['ESE', 'DBD', 'EDE'],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                D: '#forge:plates/mana_diamond',
+                E: 'rftoolsbase:infused_enderpearl',
+                S: 'forbidden_arcanus:dark_nether_star'
+            },
+            id: 'sophisticatedbackpacks:inception_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:crafting_upgrade',
+            pattern: [' T ', 'IBI', ' C '],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                C: '#forge:chests/wooden',
+                T: 'minecraft:crafting_table',
+                I: '#forge:plates/iron'
+            },
+            id: 'sophisticatedbackpacks:crafting_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:pickup_upgrade',
+            pattern: [' P ', 'SBS', 'RRR'],
+            key: {
+                B: 'sophisticatedbackpacks:smelting_upgrade',
+                R: '#forge:dusts/redstone',
+                S: 'botania:mana_string',
+                P: 'minecraft:sticky_piston'
+            },
+            id: 'sophisticatedbackpacks:pickup_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:pickup_upgrade',
+            pattern: [' E ', 'IBI', 'RCR'],
+            key: {
+                B: 'sophisticatedbackpacks:refill_upgrade',
+                R: '#forge:dusts/redstone',
+                C: '#forge:chests/wooden',
+                E: 'rftoolsbase:infused_enderpearl',
+                I: '#forge:plates/iron'
+            },
+            id: 'sophisticatedbackpacks:refill_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:restock_upgrade',
+            pattern: [' P ', 'IBI', 'RCR'],
+            key: {
+                B: 'sophisticatedbackpacks:refill_upgrade',
+                R: '#forge:dusts/redstone',
+                C: '#forge:chests/wooden',
+                P: 'minecraft:sticky_piston',
+                I: '#forge:plates/iron'
+            },
+            id: 'sophisticatedbackpacks:restock_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:pump_upgrade',
+            pattern: ['GTG', 'PBS', 'GTG'],
+            key: {
+                B: 'sophisticatedbackpacks:refill_upgrade',
+                S: 'minecraft:sticky_piston',
+                G: 'evilcraft:obscured_glass',
+                P: 'minecraft:sticky_piston',
+                T: 'mob_grinding_utils:xp_tap'
+            },
+            id: 'sophisticatedbackpacks:pump_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:tank_upgrade',
+            pattern: ['MTM', 'MBM', 'MTM'],
+            key: {
+                B: 'sophisticatedbackpacks:refill_upgrade',
+                M: 'botania:mana_glass',
+                T: 'mekanism:basic_fluid_tank'
+            },
+            id: 'sophisticatedbackpacks:tank_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:battery_upgrade',
+            pattern: ['GRG', 'RBR', 'GRG'],
+            key: {
+                B: 'sophisticatedbackpacks:refill_upgrade',
+                R: 'thermal:rf_coil',
+                G: '#forge:plates/gold'
+            },
+            id: 'sophisticatedbackpacks:battery_upgrade'
+        },
+        {
+            output: 'sophisticatedbackpacks:magnet_upgrade',
+            pattern: ['EIE', 'IBI', 'R L'],
+            key: {
+                B: 'sophisticatedbackpacks:refill_upgrade',
+                R: '#forge:dusts/redstone',
+                L: '#forge:gems/lapis',
+                I: '#forge:plates/iron',
+                E: 'rftoolsbase:infused_enderpearl'
+            },
+            id: 'sophisticatedbackpacks:magnet_upgrade'
+        },
         
     ];
 
