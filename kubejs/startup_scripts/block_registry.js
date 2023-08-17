2d663ab6cdd29921d2ff1132a65a12d6b23ec53e
Gogo08 <gogo08190@gmail.com>
2023-08-17 14:20:02 +0200

    [:bug:] Fix name color

--- kubejs/startup_scripts/block_registry.js
+++ kubejs/startup_scripts/block_registry.js
@@ -33,10 +33,10 @@ onEvent('block.registry', (event) => {
       {name: 'nebula_extractor_t2_laser_top', displayName: '§cNebula Extractor Laser Tier 2'},
       {name: 'nebula_extractor_t2_frame', displayName: '§cNebula Extractor Frame Tier 2'},
       {name: 'nebula_drill_t2', displayName: '§cNebula Drill Extractor Tier 2'},
-      {name: 'nebula_extractor_t3_laser_support', displayName: '§3Nebula Extractor Laser Support Tier 3'},
-      {name: 'nebula_extractor_t3_laser_top', displayName: '§3Nebula Extractor Laser Tier 3'},
-      {name: 'nebula_extractor_t3_frame', displayName: '§3Nebula Extractor Frame Tier 3'},
-      {name: 'nebula_drill_t3', displayName: '§3Nebula Drill Extractor Tier 3'}
+      {name: 'nebula_extractor_t3_laser_support', displayName: '§dNebula Extractor Laser Support Tier 3'},
+      {name: 'nebula_extractor_t3_laser_top', displayName: '§dNebula Extractor Laser Tier 3'},
+      {name: 'nebula_extractor_t3_frame', displayName: '§dNebula Extractor Frame Tier 3'},
+      {name: 'nebula_drill_t3', displayName: '§dNebula Drill Extractor Tier 3'}
     ];
 
     blocks.forEach((block) => {
