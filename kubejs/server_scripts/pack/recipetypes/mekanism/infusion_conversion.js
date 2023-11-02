1f3e370e226bf0569077198ac6fe63c95afc83ee
Gogo08 <gogo08190@gmail.com>
2023-11-02 13:06:17 +0100

    [:bug:] Fix infuse type

--- kubejs/server_scripts/pack/recipetypes/mekanism/infusion_conversion.js
+++ kubejs/server_scripts/pack/recipetypes/mekanism/infusion_conversion.js
@@ -43,8 +43,18 @@ onEvent('recipes', (event) => {
       },
       {
         input: { ingredient: { item: 'chroma:enriched_lunaris_nova' } },
-        output: { amount: 80, infuse_type: 'chroma:lunaris_nova' },
+        output: { amount: 80, infuse_type: 'chroma:marsova_ignis' },
         id: `${id_prefix}lunaris_nova_from_enriched`
+      },
+      {
+        input: { ingredient: { item: 'chroma:marsova_ignis' } },
+        output: { amount: 10, infuse_type: 'chroma:lunaris_nova' },
+        id: `${id_prefix}marsova_ignis`
+      },
+      {
+        input: { ingredient: { item: 'chroma:enriched_marsova_ignis' } },
+        output: { amount: 80, infuse_type: 'chroma:marsova_ignis' },
+        id: `${id_prefix}marsova_ignis_from_enriched`
       },
       {
         input: { ingredient: { item: 'chroma:enriched_compressed_steel' } },
