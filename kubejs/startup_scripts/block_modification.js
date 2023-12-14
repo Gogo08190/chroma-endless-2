603000438131543466b4348d88bddca423559cc8
Gogo08 <gogo08190@gmail.com>
2023-12-14 11:08:23 +0100

    [:bug:] Fix other compact crafting blocks

--- kubejs/startup_scripts/block_modification.js
+++ kubejs/startup_scripts/block_modification.js
@@ -1,3 +1,5 @@
 onEvent('block.tags', event => {
   event.add('minecraft:mineable/pickaxe', 'compactcrafting:field_projector')
+  event.add('minecraft:mineable/pickaxe', 'compactcrafting:match_proxy')
+  event.add('minecraft:mineable/pickaxe', 'compactcrafting:rescan_proxy')
 })
