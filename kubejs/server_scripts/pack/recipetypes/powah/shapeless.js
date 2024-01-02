d6a81564bf512183c1f391291d614b3450bfe1f6
Gogo08 <gogo08190@gmail.com>
2024-01-02 10:55:56 +0100

    [:sparkles:] Blacklist mobs spirit

--- kubejs/server_scripts/pack/recipetypes/powah/shapeless.js
+++ kubejs/server_scripts/pack/recipetypes/powah/shapeless.js
@@ -1,24 +1,12 @@
 onEvent('recipes', (event) => {
-    const id_prefix = 'chroma:pack/powah/shapeless/';
+    const id_prefix = 'chroma:pack/spirit/shapeless/';
     const recipes = [
       {
-          output: 'chroma:dielectric_paste_block',
-          inputs: ['9x powah:dielectric_paste'],
-          id: `${id_prefix}dielectric_paste_block_from_dielectric_paste`
-      },
-      {
-          output: '9x powah:dielectric_paste',
-          inputs: ['chroma:dielectric_paste_block'],
-          id: `${id_prefix}dielectric_paste_from_dielectric_paste_block`
+          output: 'spirit:soul_crystal',
+          inputs: ['spirit:soul_crystal'],
+          id: `${id_prefix}soul_crystal`
       },
     ];
-
-    powahTiers.forEach((tier) => {
-        if (tier == 'starter') {
-            return;
-        }
-        event.shapeless(`powah:reactor_${tier}`, `powah:reactor_${tier}`);
-    });
 
     recipes.forEach((recipe) => {
         event.shapeless(recipe.output, recipe.inputs).id(recipe.id);
